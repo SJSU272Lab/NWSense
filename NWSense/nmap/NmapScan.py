b8363b52d94c7b69b116812319d9c6542e189d0d
khoale88 <lenguyenkhoa1988@gmail.com>
2016-11-21 19:04:10 -0800

    create IPScan class, TimeScan feature

--- NWSense/nmap/NmapScan.py
+++ NWSense/nmap/NmapScan.py
@@ -2,9 +2,10 @@ import nmap
 from getIPAddr import IFConfig
 import os
 import re
+import time
 
 
-nm = nmap.PortScanner()
+
 ifconfig = IFConfig(None)
 
 myAddr = ifconfig.getIP()
@@ -27,21 +28,28 @@ r = re.search(pat,myAddr,re.I)
 
 ipAddr = r.group(1) + mask
 
-print ipAddr
+class IPScan():
+    def __init__(self):
+        self.nm = nmap.PortScanner()
+    
+    def getScanTime(self):
+        return self.elapsed_time
 
-def getOnlineHosts():
-    #for quick determination of online hosts only do ping scan
-    nm.scan(ipAddr,arguments='-sn')
-    results = []
-    hosts = nm.all_hosts()
-    hosts.remove(myAddr)
-    hosts.remove(gateW)
-    for h in nm.all_hosts():
-        host_mac = (h,"--:--:--:--:--:--")
-        if os.getuid() == 0 and 'mac' in nm[h]['addresses']:
-            host_mac = (h,nm[h]['addresses']['mac'])
-        results.append(host_mac)
-    return results
+    def getOnlineHosts(self):
+        #for quick determination of online hosts only do ping scan
+        start_time = time.time()
+        self.nm.scan(ipAddr,arguments='-sn')
+        self.elapsed_time = time.time() - start_time
+        results = []
+        hosts = self.nm.all_hosts()
+        hosts.remove(myAddr)
+        hosts.remove(gateW)
+        for h in hosts:
+            host_mac = (h,"--:--:--:--:--:--")
+            if os.getuid() == 0 and 'mac' in self.nm[h]['addresses']:
+                host_mac = (h,self.nm[h]['addresses']['mac'])
+            results.append(host_mac)
+        return results
 
 
 
--- NWSense/nmap/app.py
+++ NWSense/nmap/app.py
@@ -1,4 +1,4 @@
-from NmapScan import getOnlineHosts
+from NmapScan import IPScan
 import datetime
 import getIPAddr
 
@@ -7,17 +7,20 @@ import getIPAddr
 #get registered mac addresses
 registered_macs = [mac.rstrip('\n') for mac in open("registered_mac.txt",'r')]
 
-online_hosts = getOnlineHosts()
-print ('Found %s hosts up:\n' %len(online_hosts))
+ipScan = IPScan()
+online_hosts = ipScan.getOnlineHosts()
+
+print ('Found %s hosts up :\n' %len(online_hosts))
 
 #output to connection_report.txt
 output = open('connection_report.txt','a')
-output.write('Scan at ' +  datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + '\n')
-output.write('\tFound %s hosts up:\n' %len(online_hosts))
+output.write('Scan at : ' +  datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + '.\n')
+output.write('Total time scan : ' + ipScan.getScanTime() + ' seconds.\n' )
+output.write('\tFound %s hosts up :\n' %len(online_hosts))
 
 unregistered = []
 
-output.write('\t\tRegistered devices\n')
+output.write('\t\tRegistered devices :\n')
 for host in online_hosts:
     h = host[0]
     m = host[1]
@@ -27,7 +30,7 @@ for host in online_hosts:
     else:
         unregistered.append(host)
 
-output.write('\t\tSuspicious devices\n')
+output.write('\t\tSuspicious devices :\n')
 print('\n')
 for host in unregistered:
     h = host[0]
