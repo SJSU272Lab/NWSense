c72ef677d59176c2ce5988dde844fb83911253e8
Seema Rohilla <seemarohilla16@gmail.com>
2016-12-18 20:22:56 -0800

    Changes in Website
    Update in webpages

--- Updated Website/NwSense/app.py
+++ Updated Website/NwSense/app.py
@@ -1,6 +1,5 @@
 from flask import Flask,render_template,request,json
 import requests
-import urllib2
 app=Flask(__name__)
 
 @app.route("/index")
@@ -9,13 +8,26 @@ def index():
 
 @app.route("/login")
 def login():
+    userId=request.form.get('userId')
+    password=request.form.get('password')
+    response = requests.get("http://54.165.49.28:4444/authen")
+    data = json.loads(response.content)
+    #checks for authentication
     return render_template('login.html')
+    
+@app.route("/contact")
+def contact():
+    return render_template('contact.html')
 
 @app.route("/blockWebs",methods=['POST'])
 def blockWebs():
     message=["Succesfully Blocked !"]
-  
-    blockwebList=[]
+    deviceBlocked=[]
+    response = requests.get("http://54.165.49.28:4444/blockWebs/b539ab74bc9bc3e43a4b40040a66fa35")
+    data = json.loads(response.content) 
+    listdata= data.get("blockWebs")
+    
+    blockwebList=listdata
     fb1=request.form.get('fb')
     tw1=request.form.get('twitter')
     bd=request.form.get('bigdaddy')
@@ -23,40 +35,56 @@ def blockWebs():
     youtube=request.form.get('youtube')
     url=request.form.get('websitename')
 
-    if fb1 !=None:
+    if fb1 !=None and fb1 not in blockwebList :
         blockwebList.append(fb1)
-    if tw1 !=None:
+    if tw1 !=None and tw1 not in blockwebList :
         blockwebList.append(tw1)
-    if bd !=None:
+    if bd !=None and bd not in blockwebList :
         blockwebList.append(bd)
-    if mn !=None:
+    if mn !=None and mn not in blockwebList :
         blockwebList.append(mn)  
-    if youtube !=None:
+    if youtube !=None and youtube not in blockwebList :
         blockwebList.append(youtube)  
-    if url !="":
+    if url !="" and url not in blockwebList :
         blockwebList.append(url)                   
-
+   
     payload = {
 	    "userId":"b539ab74bc9bc3e43a4b40040a66fa35",
 	    "blockWebs":blockwebList
             }
     r = requests.put("http://54.165.49.28:4444/blockWebs", json=payload)
-    return render_template('blockWebsite.html', msg=message)
+    response = requests.get("http://54.165.49.28:4444/blockWebs/b539ab74bc9bc3e43a4b40040a66fa35")
+    data = json.loads(response.content) 
+    listdata1= data.get("blockWebs")
+    return render_template('blockWebsite.html', msg=message , ls=listdata1)
 
 @app.route("/blockWebsite")
 def blockWebsite():
     response = requests.get("http://54.165.49.28:4444/blockWebs/b539ab74bc9bc3e43a4b40040a66fa35")
-    print response.json
+    data = json.loads(response.content) 
+    listdata= data.get("blockWebs")
     message=[""]
-    return render_template('blockWebsite.html', msg=message)
+    return render_template('blockWebsite.html', msg=message,ls=listdata)
 
 @app.route("/blockMac",methods=['POST','GET'])
 def blockMac(): 
     message=[]
     msg=""
+    addr=[]
+    response = requests.get("http://54.165.49.28:4444/blockMacs/b539ab74bc9bc3e43a4b40040a66fa35")
+    data = json.loads(response.content) 
+    listdata= data.get("blockMacs")
+    for l in listdata:
+        addr.append(l.get("addr"))
+
     if request.method == "POST":
         selected_macs = request.form.getlist("macs")
         msg="Successfully Blocked"
+        payload = {
+	    "userId":"b539ab74bc9bc3e43a4b40040a66fa35",
+	    "blockMacs":selected_macs
+            }
+        r = requests.put("http://54.165.49.28:4444/append/blockMacs", json=payload)    
         # put request
     add=[]       
     response = requests.get("http://54.165.49.28:4444/unregMacs/b539ab74bc9bc3e43a4b40040a66fa35")
@@ -64,13 +92,35 @@ def blockMac():
     listdata= data.get("unregMacs")
     for l in listdata:
         add.append(l.get("addr"))
-    message.append(msg)    
-    return render_template('blockMacs.html', macList=add ,msg=message)
+    message.append(msg)  
+    message=[]
+    msg=""
+    addr=[]
+    response = requests.get("http://54.165.49.28:4444/blockMacs/b539ab74bc9bc3e43a4b40040a66fa35")
+    data = json.loads(response.content) 
+    listdata= data.get("blockMacs")
+    for l in listdata:
+        addr.append(l.get("addr"))  
+    return render_template('blockMacs.html', macList=add ,msg=message,ls=addr)
    
 
 @app.route("/home")
 def home():
     return render_template('home.html')
+
+@app.route("/registerMacs",methods=['POST','GET'])
+def registerMacs():
+    if request.method == "POST":
+        regMac=request.form.get('deviceMac')
+        regMacList=[]
+        regMacList.append(regMac)
+        payload = {
+	    "userId":"b539ab74bc9bc3e43a4b40040a66fa35",
+	    "regMacs":regMacList
+            }
+        r = requests.put("http://54.165.49.28:4444/append/regMacs", json=payload)  
+
+    return render_template('registerMacs.html')    
 
 @app.route("/dashboard")
 def dashboard():
